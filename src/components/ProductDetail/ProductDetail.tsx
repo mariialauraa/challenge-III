993cd7499a36666022910c4c447033d08e0912d9
Maria Laura <mlauraifg@gmail.com>
2024-07-26 17:03:14 -0300

    feat: ProductDetail component and SingleProduct page

---
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,98 @@
+import { useProducts } from "../../hooks/useProducts";
+import { useParams } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+import { Icolors } from '../../types/product_types';
+
+const ProductDetail = () => {
+    const { id } = useParams<{ id: string }>()
+    const { getSingleProduct } = useProducts()
+
+    if (!id) {
+        return <div>Product not found</div>;
+    }
+
+    const product = getSingleProduct(parseInt(id))
+
+    if (!product) {
+        return <div>Product not found</div>;
+    }
+
+    const renderStars = (rating: number) => {
+        const fullStars = Math.floor(rating)
+        const hasHalfStar = rating % 1 !== 0
+        const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0)
+
+        return (
+            <>
+                {[...Array(fullStars)].map((_, index) => (
+                    <FaStar key={index} className="text-yellow-500" />
+                ))}
+                {hasHalfStar && <FaStarHalfAlt className="text-yellow-500" />}
+                {[...Array(emptyStars)].map((_, index) => (
+                    <FaRegStar key={index} className="text-yellow-500" />
+                ))}
+            </>
+        )
+    }
+
+    const renderColor = (colors: Icolors[]) => {
+        return colors.map((color, index) => (
+            <div key={index} className="flex items-center mt-2">
+                <span className="inline-block w-6 h-6 rounded-full" style={{ backgroundColor: color.hex }}></span>
+            </div>
+        ))
+    }
+
+    return (
+        <div>
+            <div className="bg-[#F9F1E7] py-7 px-8 mb-8">
+                <p className="flex items-center gap-4 font-poppins font-normal text-base">
+                    <span className="text-gray-400">Home</span>
+                    <IoIosArrowForward />
+                    <span className="text-gray-400">Shop</span>
+                    <IoIosArrowForward />
+                    <span className="relative pl-4">
+                        <span className="absolute inset-y-0 left-0 w-0.5 bg-gray-400" />
+                        {product.title}
+                    </span>
+                </p>
+            </div>
+            <div className="flex">
+                <div className="bg-[#F9F1E7] w-[432px] h-[500px] rounded-[10px] flex items-center justify-center overflow-hidden">
+                    <img 
+                        src={product.images.mainImage} 
+                        alt={product.title} 
+                        className="w-96 h-96 object-contain"
+                    />
+                </div>
+                <div className="ml-8 font-poppins">
+                    <h1 className="text-4xl font-normal mb-2">{product.title}</h1>
+                    <span className="text-gray-400 font-medium text-2xl">R$ {product.salePrice}</span>
+                    <div className="flex items-center mt-2">
+                        {renderStars(product.rating)}
+                    </div>
+                    <p className="mt-2">{product.description.short}</p>
+                    <h3 className="mt-5 text-base font-normal text-gray-400">Size</h3>
+                    <div className="mt-5">
+                        <h3 className="text-base font-normal text-gray-400">Color</h3>
+                        <div className="flex gap-4">
+                            {renderColor(product.colors)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        // <div>
+        //     <h1>{product.title}</h1>
+        //     <img src={product.images.mainImage} alt={product.title} />
+        //     <p>{product.description.long}</p>
+        //     <p>Price: ${product.salePrice}</p>
+        //     <p>Category: {product.category}</p>
+        //     <p>Rating: {product.rating}</p>
+        //     <button>Add to Cart</button>
+        // </div>
+    );
+};
+
+export default ProductDetail;
---
+++ src/pages/SingleProduct.tsx
@@ -0,0 +1,12 @@
+import ProductDetail from "../components/ProductDetail/ProductDetail"
+
+
+const SingleProduct = () => {
+  return (
+    <div>
+        <ProductDetail />
+    </div>
+  )
+}
+
+export default SingleProduct
