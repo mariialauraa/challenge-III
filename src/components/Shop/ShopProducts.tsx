9ec770aceeaaca3f06d6135d2e99b024e87348cf
Maria Laura <mlauraifg@gmail.com>
2024-07-26 16:59:43 -0300

    fix: add Link for product:id

--- src/components/Shop/ShopProducts.tsx
+++ src/components/Shop/ShopProducts.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import productsData from '../../data/products.json'
+import productsData from '../../../public/data/products.json'
+import { Link } from 'react-router-dom'
 
 const ShopProducts = () => {
     const [currentPage, setcurrentPage] = useState(1)
@@ -21,39 +22,42 @@ const ShopProducts = () => {
                         key={product.id}
                         className='p-4 relative w-[285px] h-[446px] mx-auto'
                     >
-                        <img 
-                            src={product.images.mainImage} 
-                            alt={product.title} 
-                            className='w-full h-48 object-cover'
-                        />
-                        {product.discountPercentage > 0 && (
-                            <span className='absolute top-2 right-2 bg-red-500 text-white text-xs px-2 py-3 rounded-full'>
-                                -{Math.round(product.discountPercentage * 100)}%
-                            </span>
-                        )}
-                        {product.new && (
-                            <span className='absolute top-2 left-2 bg-green-500 text-white text-xs px-2 py-3 rounded-full'>
-                                New
-                            </span>
-                        )}
-                        <div className='p-4 bg-[#F4F5F7]'>
-                            <h3 className='font-semibold text-lg'>
-                                {product.title}
-                            </h3>
-                            <p className='text-gray-500 text-xs font-medium'>
-                                {product.description.short}
-                            </p>
-                            <div className='flex items-center space-x-4 mt-4'>
-                                <span className='font-semibold text-lg'>
-                                    R$ {product.salePrice.toFixed(2)}
+                        <Link to={`/product/${product.id}`} className='block h-full'>
+                            <img 
+                                src={product.images.mainImage} 
+                                alt={product.title} 
+                                className='w-full h-48 object-cover'
+                            />
+                            {product.discountPercentage > 0 && (
+                                <span className='absolute top-2 right-2 bg-red-500 text-white text-xs px-2 py-3 rounded-full'>
+                                    -{Math.round(product.discountPercentage * 100)}%
                                 </span>
-                                {product.normalPrice && (
-                                    <span className='font-normal text-xs text-gray-400 line-through'>
-                                        R$ {product.normalPrice.toFixed(2)}
+                            )}
+                            {product.new && (
+                                <span className='absolute top-2 left-2 bg-green-500 text-white text-xs px-2 py-3 rounded-full'>
+                                    New
+                                </span>
+                            )}
+                            <div className='p-4 bg-[#F4F5F7]'>
+                                <h3 className='font-semibold text-lg'>
+                                    {product.title}
+                                </h3>
+                                <p className='text-gray-500 text-xs font-medium'>
+                                    {product.description.short}
+                                </p>
+                                <div className='flex items-center space-x-4 mt-4'>
+                                    <span className='font-semibold text-lg'>
+                                        R$ {product.salePrice.toFixed(2)}
                                     </span>
-                                )}
+                                    {product.normalPrice && (
+                                        <span className='font-normal text-xs text-gray-400 line-through'>
+                                            R$ {product.normalPrice.toFixed(2)}
+                                        </span>
+                                    )}
+                                </div>
                             </div>
-                        </div>
+                        </Link>
+                        
                     </div>
                 ))}
             </div>
